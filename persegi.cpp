1eb43768999a900a1dee3b27e9448f644798b00c
pichiefox <citrazad@gmail.com>
2026-02-24 15:48:11 +0700

    first commit

---
+++ persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{//start
+ float p,l,luas;
+
+ //langkah 2
+ cout << "Masukkan Panjang : " ;
+ cin >> p;
+  //langkah 3 : Masukkan Lebar
+ cout << "Masukkan Lebar : " ;
+ cin >> p;
+  //langkah 4 : Hitung Luas = p x l
+ luas = p*l;
+ //langkah 5
+ cout << "Hasilnya = " << luas;
+}//selesai
