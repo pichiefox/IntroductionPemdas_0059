40c2a03bc9950a2b4b70b520de9cd655b2300b31
pichiefox <citrazad@gmail.com>
2026-03-03 15:43:45 +0700

    Praktikum 2

---
+++ Praktikum2.cpp
@@ -0,0 +1,33 @@
+//Library
+#include <iostream>
+using namespace std;
+//Deklarasi Global
+    float p,l;
+
+//Implementasi Prosedur & Fungsi
+void Input(){
+
+cout << "Masukan Panjang : ";
+cin >> p;
+cout << "Masukkan Lebar : ";
+cin >> l;
+}
+
+float LuasPersegi(float x, float y){
+    return x*y;
+}
+
+int Jumlah(int a, int b, int c){
+    return a+b+c;
+}
+
+void Output(){
+    cout <<"Hasilnya = " << LuasPersegi(p,l) <<endl;
+}
+//Program Utama
+int main()
+{//start
+    Input();
+    Output();
+    cout << "Hasil Penjumlahan = " << Jumlah (5,6,9);
+}//selesai
