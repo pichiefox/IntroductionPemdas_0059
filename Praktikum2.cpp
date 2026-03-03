ce7e416d6dca329c854d1376bd1243da03a1176f
pichiefox <citrazad@gmail.com>
2026-03-03 15:45:17 +0700

    3

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -4,3 +4,12 @@ using namespace std;
 //Deklarasi Global
     float p,l;
 
+//Implementasi Prosedur & Fungsi
+void Input(){
+
+cout << "Masukan Panjang : ";
+cin >> p;
+cout << "Masukkan Lebar : ";
+cin >> l;
+}
+
