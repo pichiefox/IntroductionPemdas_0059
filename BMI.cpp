0e1ffcdb2b7af0d8c5f5035f92c8de72d983c01d
pichiefox <citrazad@gmail.com>
2026-03-11 09:44:55 +0700

    BMI.cpp Done

--- BMI.cpp
+++ BMI.cpp
@@ -29,5 +29,11 @@ string statusBMI (float bmi)
 
 int main()
 {
+    inputData();
+
+    float bmi = hitungBMI (berat, tinggi);
+    cout<< "\nBMI Anda: " <<bmi<<endl;
+    cout<< "Status :"<<statusBMI(bmi)<<endl;
     
+    return 0;
 }
