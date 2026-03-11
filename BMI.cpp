a244f89425289de65b778aa5530adcb091544678
pichiefox <citrazad@gmail.com>
2026-03-11 09:31:36 +0700

    Input Tinggi (m)

--- BMI.cpp
+++ BMI.cpp
@@ -8,5 +8,6 @@ void inputData()
     cout << "--- Selamat Datang Mahasiswa Kelas B 2025---" << endl;
     cout << "Masukkan Berat Badan (kg): ";
     cin >>berat;
-
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >>tinggi;
 }
